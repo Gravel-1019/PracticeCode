f71c2abe20e7a0fbdf334878916305ebf945ead2
Gravel <3575160075@qq.com>
2024-05-31 01:53:38 +0800

    2024.5.31

---
+++ Java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 18;
+        String name = "John Doe";
+        System.out.println("我今年"+age);
+        System.out.println("我叫"+name);
+    }
+}
--- Java/Java/src/helloworld.java
+++
@@ -1,8 +0,0 @@
-public class helloworld {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
-        int a = 10;
-        System.out.println(a);
-    }
-}
-
